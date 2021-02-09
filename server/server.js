20e33ee5199fb2c6360f75c5b6b36ff58fe7b510
Jose Disla <josec.developer@gmail.com>
2021-02-09 04:00:04 -0500

    server.js file update

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.start = function() {
     app.emit('started');
     const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
+    console.log('Hi!');
     if (app.get('loopback-component-explorer')) {
       const explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
